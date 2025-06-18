a5db96aa7685fd9c1bd2b6bcc19e4ef5d1da5a0e
Ian Fijolek <ian@iamthefij.com>
2025-06-18 10:17:08 -0700

    Update base image and get itests working again

--- itest/run.sh
+++ itest/run.sh
@@ -5,17 +5,17 @@ cd "$(dirname "$0")"
 mkdir -p ./repo ./data
 
 echo Clean everything
-docker-compose down -v
+docker-compose down -v --remove-orphans
 rm -fr ./repo/* ./data/*
 sleep 5
 
 echo Boostrap databases and data
 docker-compose up -d mysql postgres
-docker-compose run bootstrap
+docker-compose run --rm bootstrap
 sleep 1
 
 echo Run backup job
-docker-compose run main -backup IntegrationTest -once /test-backup.hcl
+docker-compose run --rm main -backup IntegrationTest -once /test-backup.hcl
 
 echo Clean data
 docker-compose down -v
@@ -24,12 +24,12 @@ rm -fr ./data/*
 sleep 15
 
 echo Run restore
-docker-compose run main -restore IntegrationTest -once /test-backup.hcl
+docker-compose run --rm main -restore IntegrationTest -once /test-backup.hcl
 sleep 1
 
 echo Validate data
-docker-compose run validate
+docker-compose run --rm validate
 
 echo Clean all again
-docker-compose down -v
+docker-compose down -v --remove-orphans
 rm -fr ./repo/* ./data/*
