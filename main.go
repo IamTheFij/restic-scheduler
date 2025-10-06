e8f882d6596ef822fcb0a8219b81acb311c8c4cb
Ian Fijolek <ian@iamthefij.com>
2025-10-05 20:46:25 -0700

    Move config parsing to it's own file

--- main.go
+++ main.go
@@ -5,14 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 	"os/exec"
 	"strings"
-
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsimple"
-	"github.com/zclconf/go-cty/cty"
-	"github.com/zclconf/go-cty/cty/function"
 )
 
 var (
@@ -20,69 +14,6 @@ var (
 	version        = "dev"
 	ErrJobNotFound = errors.New("jobs not found")
 )
-
-func ParseConfig(path string) ([]Job, error) {
-	var config Config
-
-	ctx := hcl.EvalContext{
-		Variables: nil,
-		Functions: map[string]function.Function{
-			"env": function.New(&function.Spec{
-				Params: []function.Parameter{{
-					Name:             "var",
-					Type:             cty.String,
-					AllowNull:        false,
-					AllowUnknown:     false,
-					AllowDynamicType: false,
-					AllowMarked:      false,
-				}},
-				VarParam: nil,
-				Type:     function.StaticReturnType(cty.String),
-				Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
-					return cty.StringVal(os.Getenv(args[0].AsString())), nil
-				},
-			}),
-			"readfile": function.New(&function.Spec{
-				Params: []function.Parameter{{
-					Name:             "path",
-					Type:             cty.String,
-					AllowNull:        false,
-					AllowUnknown:     false,
-					AllowDynamicType: false,
-					AllowMarked:      false,
-				}},
-				VarParam: nil,
-				Type:     function.StaticReturnType(cty.String),
-				Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
-					content, err := os.ReadFile(args[0].AsString())
-					if err != nil {
-						return cty.StringVal(""), err
-					}
-
-					return cty.StringVal(string(content)), nil
-				},
-			}),
-		},
-	}
-
-	if err := hclsimple.DecodeFile(path, &ctx, &config); err != nil {
-		return nil, fmt.Errorf("%s: Failed to decode file: %w", path, err)
-	}
-
-	if len(config.Jobs) == 0 {
-		log.Printf("%s: No jobs defined in file", path)
-
-		return []Job{}, nil
-	}
-
-	for _, job := range config.Jobs {
-		if err := job.Validate(); err != nil {
-			return nil, fmt.Errorf("%s: Invalid job: %w", path, err)
-		}
-	}
-
-	return config.Jobs, nil
-}
 
 func ReadJobs(paths []string) ([]Job, error) {
 	allJobs := []Job{}
