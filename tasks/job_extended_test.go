93d0e1e0af100d241a5a4fa1991ddb66acfee154
Ian Fijolek <ian@iamthefij.com>
2026-08-24 21:27:22 -0700

    Add more robust test for jobs

--- tasks/job_extended_test.go
+++ tasks/job_extended_test.go
@@ -1,10 +1,50 @@
 package tasks_test
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"git.iamthefij.com/iamthefij/restic-scheduler/config"
 	"git.iamthefij.com/iamthefij/restic-scheduler/tasks"
+	"git.iamthefij.com/iamthefij/restic-scheduler/utils"
 	"github.com/stretchr/testify/assert"
+)
+
+const (
+	TestJobHCL = `
+job "TestJob" {
+	schedule = "* * * * *"
+
+	config {
+		repo = "%s"
+		passphrase = "shhh"
+
+		options {
+			CacheDir = "%s"
+		}
+	}
+
+
+	backup {
+		paths = ["%s"]
+
+		restore_opts {
+			Target = "%s"
+		}
+	}
+
+	copy {
+		target_repo = "%s"
+		target_passphrase = "shhh"
+	}
+
+	forget {
+		KeepLast = 2
+	}
+}
+`
 )
 
 func TestAllTasks(t *testing.T) {
@@ -153,4 +193,95 @@ func TestNewRestic(t *testing.T) {
 	assert.Equal(t, resticCfg.Repo, restic.Repo)
 	assert.Equal(t, resticCfg.Passphrase, restic.Passphrase)
 	assert.Equal(t, resticCfg.Env, restic.Env)
+}
+
+func TestJobBackups(t *testing.T) {
+	t.Parallel()
+
+	if testing.Short() {
+		t.Skip("Skip integration test when running short tests")
+	}
+
+	dataDir := t.TempDir()
+	repoDir := t.TempDir()
+	cacheDir := t.TempDir()
+	restoreTarget := t.TempDir()
+	repoCopyDir := t.TempDir()
+
+	dataFile := filepath.Join(dataDir, "test.txt")
+	restoredDataFile := filepath.Join(restoreTarget, dataFile)
+
+	mainJobHCL := fmt.Sprintf(TestJobHCL, repoDir, cacheDir, dataDir, restoreTarget, repoCopyDir)
+	jobs, err := config.ParseConfig("test.hcl", []byte(mainJobHCL))
+	utils.AssertEqualFail(t, "unexpected error parsing job hcl", nil, err)
+
+	testJob := jobs[0]
+
+	// Write test file to the data dir
+	err = os.WriteFile(dataFile, []byte("testing"), 0o644)
+	utils.AssertEqualFail(t, "unexpected error writing to test file", nil, err)
+
+	// Run initial backup, should auto initialize repo
+	err = testJob.RunBackup()
+	utils.AssertEqualFail(t, "unexpected error running backups", nil, err)
+
+	// Check snapshots
+	r := testJob.NewRestic()
+	snapshots, err := r.ReadSnapshots()
+	utils.AssertEqualFail(t, "unexpected error reading snapshots", nil, err)
+	utils.AssertEqual(t, "unexpected number of snapshots", 1, len(snapshots))
+
+	// Backup again
+	err = testJob.RunBackup()
+	utils.AssertEqualFail(t, "unexpected error running backups", nil, err)
+
+	// Check for second backup
+	snapshots, err = r.ReadSnapshots()
+	utils.AssertEqualFail(t, "unexpected error reading second snapshots", nil, err)
+	utils.AssertEqual(t, "unexpected number of snapshots", 2, len(snapshots))
+
+	// Run third backup to trigger a prune
+	err = testJob.RunBackup()
+	utils.AssertEqualFail(t, "unexpected error running backups", nil, err)
+
+	// Check only 2 backups
+	snapshots, err = r.ReadSnapshots()
+	utils.AssertEqualFail(t, "unexpected error reading second snapshots", nil, err)
+	utils.AssertEqual(t, "unexpected number of snapshots", 2, len(snapshots))
+
+	// Restore files
+	err = testJob.RunRestore("latest")
+	utils.AssertEqualFail(t, "unexpected error restoring latest snapshot", nil, err)
+
+	// Check restored values
+	value, err := os.ReadFile(restoredDataFile)
+	utils.AssertEqualFail(t, "unexpected error reading from test file", nil, err)
+	utils.AssertEqualFail(t, "incorrect value in test file", "testing", string(value))
+
+	// Try to unlock the repo (repo shouldn't really be locked, but this should still run without error
+	err = testJob.RunUnlock()
+	utils.AssertEqualFail(t, "unexpected error unlocking repo", nil, err)
+
+	// Clear out test job so we don't accidentally use it below for restore operations
+	testJob = tasks.Job{}
+
+	// Test restore from copy
+	copyRepoHCL := fmt.Sprintf(TestJobHCL, repoCopyDir, cacheDir, dataDir, restoreTarget, repoDir)
+	jobs, err = config.ParseConfig("test.hcl", []byte(copyRepoHCL))
+	utils.AssertEqualFail(t, "unexpected error parsing copy repo hcl", nil, err)
+
+	copyRepoJob := jobs[0]
+
+	// Remove test restored file
+	err = os.Remove(restoredDataFile)
+	utils.AssertEqualFail(t, "unexpected error removing test data file", nil, err)
+
+	// Restore files
+	err = copyRepoJob.RunRestore("latest")
+	utils.AssertEqualFail(t, "unexpected error restoring latest snapshot", nil, err)
+
+	// Check restored values
+	value, err = os.ReadFile(restoredDataFile)
+	utils.AssertEqualFail(t, "unexpected error reading from test file", nil, err)
+	utils.AssertEqualFail(t, "incorrect value in test file", "testing", string(value))
 }
